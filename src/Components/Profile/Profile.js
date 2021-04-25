16615bd34679ad4bb913a974eeea2518f0245706
Marie <marie.marcos25@gmail.com>
2021-04-25 13:41:49 -0700

    feat: profile: added styling

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import './Profile.css';
 import { withAuth0 } from '@auth0/auth0-react';
 
 class Profile extends React.Component {
@@ -7,10 +9,19 @@ class Profile extends React.Component {
     return (
       <>
         {isAuthenticated ?
-          <div>
-            <img src={user.picture} alt={user.name} />
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
+          <div class='profile-card'>
+            <Card style={{ width: '18rem' }}>
+              <div class='profile-img'>
+                <Card.Img class='img' src={user.picture} />
+              </div>
+              <Card.Body class='profile-body'>
+                <Card.Title>{user.name}</Card.Title>
+                <h6>{user.email}</h6>
+                <Card.Text>
+                Lorem ipsum dolor sit amet elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </Card.Text>
+              </Card.Body>
+            </Card>
           </div> : ''}
       </>
     )
